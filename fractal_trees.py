35c3b2596af45315e571824c6782730b1170ee79
Szallos Kis Csaba <szalloskiscsaba@gmail.com>
2021-07-23 11:54:53 +0300

    fractal tree is being built.

--- fractal_trees.py
+++ fractal_trees.py
@@ -1,5 +1,5 @@
 from p5 import *
-from random import randrange
+from random import randint
 
 
 class branch:
@@ -9,9 +9,32 @@ class branch:
         self.x_end = x_end
         self.y_end = y_end
 
-    def draw_branch(self, r, g, b):
+    def draw(self, r, g, b):
         stroke(r, g, b)
+        stroke_weight(4)
         line(self.x_start, self.y_start, self.x_end, self.y_end)
+
+    def right_child(self):
+        direction_x = self.x_end - self.x_start
+        direction_y = self.y_end - self.y_start
+        new_branch = Vector(direction_x, direction_y)
+        new_branch.rotate(PI / 4)
+        new_branch = new_branch * 0.67
+        new_x = self.x_end + new_branch.x
+        new_y = self.y_end + new_branch.y
+        right = branch(self.x_end, self.y_end, new_x, new_y)
+        return right
+
+    def left_child(self):
+        direction_x = self.x_end - self.x_start
+        direction_y = self.y_end - self.y_start
+        new_branch = Vector(direction_x, direction_y)
+        new_branch.rotate(-PI / 4)
+        new_branch = new_branch * 0.67
+        new_x = self.x_end + new_branch.x
+        new_y = self.y_end + new_branch.y
+        left = branch(self.x_end, self.y_end, new_x, new_y)
+        return left
 
 
 angle = 0
@@ -22,18 +45,32 @@ b = 255
 
 
 def setup():
-    size(1200, 1200)
+    size(600, 600)
+    root = branch(width / 2, height, width / 2, height - 200)
+    tree.append(root)
+    right_branch = root.right_child()
+    tree.append(right_branch)
+    left_branch = root.left_child()
+    tree.append(left_branch)
 
 
 def draw():
     background(51)
-    root = branch(width / 2, height, width / 2, height - 100)
-    if mouse_is_pressed:
-        r = randrange(255)
-        g = randrange(255)
-        b = randrange(255)
 
-    root.draw_branch(r, g, b)
+    if mouse_is_pressed:
+        g = randint(150, 255)
+        r = randint(0, g - 20)
+        b = randint(0, g - 20)
+        if len(tree) < 100:
+            tree_size = len(tree)
+            for i in range(0, tree_size):
+                left_child = tree[i].left_child()
+                tree.append(left_child)
+                right_child = tree[i].right_child()
+                tree.append(right_child)
+
+    for branch in tree:
+        branch.draw(r, g, b)
 
 
 run()
