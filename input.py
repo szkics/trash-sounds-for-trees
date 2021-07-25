967d69fbf4f8d321f9c8820c5224d68a6fde100a
Szallos Kis Csaba <szalloskiscsaba@gmail.com>
2021-07-26 00:14:51 +0300

    updated input handling for more sensors. sound sample.

--- input.py
+++ input.py
@@ -8,12 +8,31 @@ mpr121 = adafruit_mpr121.MPR121(i2c)
 
 zero_has_changed = 1
 one_has_changed = 1
+two_has_changed = 1
+three_has_changed = 1
+
 while True:
-    if mpr121[0].value and (zero_has_changed == 1):
-        print("0")
-        one_has_changed = 1
+    if mpr121[0].value and zero_has_changed == 1:
+        print "0"
         zero_has_changed = 0
-    if mpr121[1].value and (one_has_changed == 1):
-        print("1")
+        one_has_changed = 1
+        two_has_changed = 1
+        three_has_changed = 1
+    if mpr121[1].value and one_has_changed == 1:
+        print "1"
         zero_has_changed = 1
         one_has_changed = 0
+        two_has_changed = 1
+        three_has_changed = 1
+    if mpr121[2].value and two_has_changed == 1:
+        print "2"
+        zero_has_changed = 1
+        one_has_changed = 1
+        two_has_changed = 0
+        three_has_changed = 1
+    if mpr121[3].value and three_has_changed == 1:
+        print "3"
+        zero_has_changed = 1
+        one_has_changed = 1
+        two_has_changed = 1
+        three_has_changed = 0
